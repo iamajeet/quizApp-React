5a1b7869c57e4e318077738f517f949b95f84989
AjeetY <ajeety@hcl.com>
2019-07-23 18:58:13 +0530

    learn more button to www.reactjs.org

--- src/container/Jumbotron/Jumbotron.js
+++ src/container/Jumbotron/Jumbotron.js
@@ -8,7 +8,7 @@ class Jumbotron extends Component {
                 <p className="lead">This is a simple quiz to test the basics of React.js.</p>
                 <hr className="my-4" />
                 <p>Answer the folowing questions and check your result.</p>
-                <a className="btn btn-primary btn-lg" target="blank" href="#" role="button">Learn more</a>
+                <a className="btn btn-primary btn-lg" target="_blank" href="http://www.reactjs.org" role="button">Learn more</a>
             </div>
 
         );
